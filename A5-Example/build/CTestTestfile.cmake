b712cab4ed294e9603329a8e5d3ff7513ff9219a
Ubuntu <muhmmad@student.chalmers.se>
2024-03-28 02:35:17 +0100

    add build folder & commets to .PrimeChecker.cpp

---
+++ A5-Example/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/A6/2024-group-08/A5-Example
+# Build directory: /home/ubuntu/A6/2024-group-08/A5-Example/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(helloworld-Runner "/home/ubuntu/A6/2024-group-08/A5-Example/build/helloworld-Runner")
+set_tests_properties(helloworld-Runner PROPERTIES  _BACKTRACE_TRIPLES "/home/ubuntu/A6/2024-group-08/A5-Example/CMakeLists.txt;8;add_test;/home/ubuntu/A6/2024-group-08/A5-Example/CMakeLists.txt;0;")
