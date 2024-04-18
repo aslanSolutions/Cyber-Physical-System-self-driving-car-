1d12a244fc5b4a358b989f94455bc093adb0f513
M.Ali Elhasan <guselhmu@student.gu.se>
2024-04-18 14:22:40 +0200

    Remove build direction
    I have delete the build direction where I will build in the docker image
    
    Author: M.Ali

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mali/Desktop/Cyber System/Gitlab-Repo/2024-group-08/A5-Example
-# Build directory: /home/mali/Desktop/Cyber System/Gitlab-Repo/2024-group-08/A5-Example/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(helloworld-Runner "/home/mali/Desktop/Cyber System/Gitlab-Repo/2024-group-08/A5-Example/build/helloworld-Runner")
-set_tests_properties(helloworld-Runner PROPERTIES  _BACKTRACE_TRIPLES "/home/mali/Desktop/Cyber System/Gitlab-Repo/2024-group-08/A5-Example/CMakeLists.txt;8;add_test;/home/mali/Desktop/Cyber System/Gitlab-Repo/2024-group-08/A5-Example/CMakeLists.txt;0;")
